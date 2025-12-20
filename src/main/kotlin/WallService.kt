58b4e623db1866f04e2b5bf34716078ddb501542
Ivan Zhukov <36888051+vanwise@users.noreply.github.com>
2025-12-20 23:07:31 +0600

    feat: add new methods to wall service

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,3 +1,19 @@
 object WallService {
+    private var nextPostId = 0
     private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        posts += post.copy(id = nextPostId++)
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, currentPost) in posts.withIndex()) {
+            if (currentPost.id == post.id) {
+                posts[index] = post.copy()
+                return true
+            }
+        }
+        return false
+    }
 }
