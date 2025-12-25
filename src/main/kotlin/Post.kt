05b51453abc2d041ac810d76a387ff8f2e4f647f
Ivan Zhukov <36888051+vanwise@users.noreply.github.com>
2025-12-25 18:44:56 +0600

    feat: make some post fields as nullable

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,13 +1,13 @@
 data class Post(
     val id: Int,
-    val ownerId: Int,
-    val fromId: Int,
+    val ownerId: Int?,
+    val fromId: Int?,
     val createdBy: Int,
     val date: Int,
     val text: String = "",
     val replyOwnerId: Int,
     val replyPostId: Int,
-    val friendsOnly: Boolean,
+    val friendsOnly: Boolean?,
     val likes: Likes,
     val isFavourite: Boolean = false
 )
