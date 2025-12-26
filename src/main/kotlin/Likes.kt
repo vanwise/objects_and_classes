61945e7b332124dd657c2ca3c0ca9207acad8369
Ivan Zhukov <36888051+vanwise@users.noreply.github.com>
2025-12-26 17:02:42 +0600

    feat: add post attachments

--- src/main/kotlin/Likes.kt
+++ src/main/kotlin/Likes.kt
@@ -1,6 +1,4 @@
-data class Likes(
-    val count: Int,
-    val userLikes: Boolean,
-    val canLike: Boolean,
-    val canPublish: Boolean
+abstract class Likes(
+    open val count: Int,
+    open val userLikes: Boolean,
 )
